bc24eaf428f4809262b3cb03e03bcbe6118212ee
Ivan Zvieriev <ivan_zvieriev@epam.com>
2020-09-02 09:53:26 +0200

    Adding day 2

--- commands.sh
+++ commands.sh
@@ -111,3 +111,87 @@ docker search activemq
 docker pull webcenter/activemq
 
 docker run -i -t webcenter/activemq /bin/bash
+
+#Day 2
+docker run -i -t --name first_image ubuntu /bin/bash
+
+apt-get -yqq update
+apt-get -y install apache2
+exit
+
+docker commit -m "Image with apache installed" -a "Ivan Zvieriev" first_image ivanzvieriev/first_image
+docker push ivanzvieriev/first_image
+
+docker start first_image
+docker attach first_image
+vim
+apt-get install vim
+
+docker commit -m "Image with apache installed + vim" -a "Ivan Zvieriev" first_image ivanzvieriev/first_image:apache_vim
+docker push ivanzvieriev/first_image
+
+docker images
+docker rmi ivanzvieriev/first_image
+docker rmi ivanzvieriev/first_image:apache_vim
+
+#optional
+docker run -i -t ivanzvieriev/first_image:apache_vim /bin/bash
+
+docker rmi -f (docker images -a -q)
+
+cd static_web
+docker build --tag ivanzvieriev/static_web .
+
+docker images
+docker history ivanzvieriev/static_web
+docker push ivanzvieriev/static_web
+#(check on dockerhub)
+
+docker run -d -p 80 --name static_web ivanzvieriev/static_web nginx -g "daemon off;"
+docker port static_web
+#http://localhost:32768/ (check in browser)
+
+# Extend our Dockerfile with lines
+# RUN echo 'New line’ >> /var/www/html/index.html
+
+docker build --tag ivanzvieriev/static_web:v2 .
+docker push ivanzvieriev/static_web:v2
+docker run -d -p 80 --name static_web2 ivanzvieriev/static_web:v2 nginx -g "daemon off;"
+
+docker run -d -p 80:80 --name static_web3 ivanzvieriev/static_web:v2 nginx -g "daemon off;"
+# check in browser - localhost
+
+#Modify version in the Dockerfile
+docker build --tag ivanzvieriev/static_web:v3 .
+
+cd copy_static_web
+docker build --tag ivanzvieriev/copy_static_web .
+docker stop (docker ps -a -q)
+
+# github.com
+# create repository git_web
+# git clone https://github.com/ivan-zvieriev/git_web.git
+# copy Docker and index.html files
+git status
+git add .
+git commit -m "Adding index.html and Dockerfile"
+git push
+
+docker build --tag ivanzvieriev/git_web https://github.com/ivan-zvieriev/git_web.git
+docker stop (docker ps -q)
+docker run -d -p 80:80 --name git_web ivanzvieriev/git_web nginx -g "daemon off;"
+docker push ivanzvieriev/git_web
+
+# Go to repository git_web
+# Authorize github
+# Configure auto_build
+#
+# Modify and push changes to both Dockerfile and index.html
+# Verify that automated build has been triggered
+
+# Modify index.html
+git add .
+git commit -m „Preparing release 1.0.2”
+git push
+git tag 1.0.2
+git push origin --tags
